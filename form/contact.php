8e3a7f36261050074dc74e4caaf5a1acaf426dd2
yintong-zhou <zhouyintong96@gmail.com>
2023-03-28 12:09:52 +0200

    add form

---
+++ form/contact.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = 'assistenza@mlsistemi.it'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Contatto dal Web Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@mlsistemi.it\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+echo "Your message has been sent";
+?>
