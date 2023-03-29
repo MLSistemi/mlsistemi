35d65b7b837c82ea1db756cbcaaf40ac7bcf8ae7
yintong-zhou <zhouyintong96@gmail.com>
2023-03-29 13:56:35 +0200

    update form email

--- form/contact.php
+++ form/contact.php
@@ -5,11 +5,11 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = 'assistenza@mlsistemi.it'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@mlsistemi.it'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@mlsistemi.it\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-echo "Messaggio Inviato";
+echo "<h3>Messaggio Inviato</h3>";
 ?>
